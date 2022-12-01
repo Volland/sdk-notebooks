ed7c06b6728f43c25928e002a84ab5d6262abc48
Volodymyr Pavlyshyn <volodymyr.p@affinidi.org>
2022-12-01 19:40:10 +0100

    diagram

--- sdk-create-wallet-demo.ts
+++ sdk-create-wallet-demo.ts
@@ -1,7 +1,7 @@
 import { createWallet } from '@affinidi/wallet-node-sdk'
 import { EventComponent } from '@affinidi/wallet-core-sdk'
 const main = async () => {
-const walletFactory = createWallet('AffinityCore' as EventComponent)
+const walletFactory = createWallet(EventComponent.AffinidiCore)
 const accessApiKey = '3794c3f8d9fb9a92770d79dbd5e490bc22bb0e58e9ff42475b379e35a575d13d'
 const password = 'P@55w0rd!!!'
 const options = {
@@ -11,11 +11,11 @@ const options = {
 const sdk = await walletFactory.createWallet(options, password)
 const seed = sdk.encryptedSeed 
 const did = sdk.did  
-
-console.log({seed , did})
+console.log('create wallet',{seed , did})
 
 const sdkOpen = await walletFactory.openWalletByEncryptedSeed(options, seed , password)
-const didDoc = await sdkOpen.resolveDid(did)
+console.log('open wallet',{did:sdkOpen.did})
+const didDoc = await sdkOpen.resolveDid(sdkOpen.did)
 console.log(JSON.stringify(didDoc, null, 2))
 }
 
