5d341fab0eb8431a033d7d638838f587d392aaa0
Volodymyr Pavlyshyn <volodymyr.p@affinidi.org>
2022-12-01 10:46:39 +0100

    fix examples

--- sdk-create-wallet-demo.ts
+++ sdk-create-wallet-demo.ts
@@ -1,35 +1,22 @@
 import { createWallet } from '@affinidi/wallet-node-sdk'
-import fetch from 'node-fetch' 
-
+import { EventComponent } from '@affinidi/wallet-core-sdk'
 const main = async () => {
-const walletFactory = createWallet('AffinityCore')
-const accessApiKey = '<your key>'
+const walletFactory = createWallet('AffinityCore' as EventComponent)
+const accessApiKey = '3794c3f8d9fb9a92770d79dbd5e490bc22bb0e58e9ff42475b379e35a575d13d'
 const password = 'P@55w0rd!!!'
 const options = {
-    env: 'prod',
+    env: 'prod' as 'prod' | 'staging' | 'dev',
     accessApiKey: accessApiKey
 }
 const sdk = await walletFactory.createWallet(options, password)
 const seed = sdk.encryptedSeed 
 const did = sdk.did  
 
-const sdkOpen = await createWallet('test').openWalletByEncryptedSeed(options, seed , password)
+console.log({seed , did})
 
-const resolveDid = async () => {
-
-const resolverUrl = `https://affinity-registry.${options.env}.affinity-project.org/api/v1/did/resolve-did`
-const response = await fetch(resolverUrl, {
-    method: 'post',
-    body: JSON.stringify({did: did}),
-    headers: {
-        'Content-Type': 'application/json',
-        'Api-Key': options.accessApiKey
-             }
-});
-didDoc = await response.json()     
-  
-}
-resolveDid().catch(console.error)
+const sdkOpen = await walletFactory.openWalletByEncryptedSeed(options, seed , password)
+const didDoc = await sdkOpen.resolveDid(did)
+console.log(JSON.stringify(didDoc, null, 2))
 }
 
 main().catch(console.error)
