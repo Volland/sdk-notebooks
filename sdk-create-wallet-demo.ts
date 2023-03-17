882025d3dfc37a1682d3fac00cdd7dfc192f5918
Volodymyr Pavlyshyn <pavlyshyn@gmail.com>
2023-03-17 11:30:43 +0100

    pex sample

--- sdk-create-wallet-demo.ts
+++ sdk-create-wallet-demo.ts
@@ -2,7 +2,7 @@ import { createWallet } from '@affinidi/wallet-node-sdk'
 import { EventComponent } from '@affinidi/wallet-core-sdk'
 const main = async () => {
 const walletFactory = createWallet(EventComponent.AffinidiCore)
-const accessApiKey = '3794c3f8d9fb9a92770d79dbd5e490bc22bb0e58e9ff42475b379e35a575d13d'
+const accessApiKey = '<your key use affinidi cli to generate a key>'
 const password = 'P@55w0rd!!!'
 const options = {
     env: 'prod' as 'prod' | 'staging' | 'dev',
